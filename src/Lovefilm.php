b547988f4708c37d1cba4345999e056afec49246
Craig Duncan <git@duncanc.co.uk>
2014-06-04 21:30:31 +0100

    Created a Lovefilm class based on OAuth

---
+++ src/Lovefilm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace duncan3dc\OAuth;
+use \duncan3dc\Helpers\Helper;
+
+class Lovefilm extends OAuth {
+
+    protected $user;
+
+
+    public function __construct($options=false) {
+
+        $options = Helper::getOptions($options,[
+            "authkey"   =>  "",
+            "secret"    =>  "",
+        ]);
+
+        parent::__construct([
+            "type"          =>  "lovefilm",
+            "requestUrl"    =>  "http://openapi.lovefilm.com/oauth/request_token",
+            "accessUrl"     =>  "http://openapi.lovefilm.com/oauth/access_token",
+            "authkey"       =>  $options["authkey"],
+            "secret"        =>  $options["secret"],
+        ]);
+
+    }
+
+
+    public function getUser() {
+
+        if($this->user) {
+            return $this->user;
+        }
+
+        $data = $this->fetch("http://openapi.lovefilm.com/users");
+        foreach($data["resource"]["links"] as $link) {
+            if($link["title"] == "current user") {
+                $this->user = $link["href"];
+            }
+        }
+
+        return $this->user;
+
+    }
+
+
+    public function getData($type=false) {
+
+        $url = $this->getUser();
+        if($type) {
+            $url .= "/" . $type;
+        }
+
+        return $this->fetch($url);
+
+    }
+
+
+}
