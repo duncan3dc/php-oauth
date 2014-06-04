e5a5cb1e0f260afd008d07382281048ea799d870
Craig Duncan <git@duncanc.co.uk>
2014-06-04 21:31:12 +0100

    Created an Instagram class based on OAuth2

---
+++ src/Instagram.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace duncan3dc\OAuth;
+use \duncan3dc\Helpers\Helper;
+use \duncan3dc\Helpers\Json;
+
+class Instagram extends OAuth2 {
+
+
+	public function __construct($options) {
+
+		$options = Helper::getOptions($options,[
+			"client"	=>	"",
+			"secret"	=>	"",
+			"username"	=>	"",
+		]);
+
+		parent::__construct([
+			"type"			=>	"instagram",
+			"client"		=>	$options["client"],
+			"secret"		=>	$options["secret"],
+			"username"		=>	$options["username"],
+			"authoriseUrl"	=>	"https://api.instagram.com/oauth/authorize",
+			"redirectUrl"	=>	"https://api.instagram.com/oauth/redirect",
+			"accessUrl"		=>	"https://api.instagram.com/oauth/access_token",
+		]);
+
+	}
+
+
+	public function timeline($options=false) {
+
+		$options = Helper::getOptions($options,[
+			"min"	=>	false,
+			"limit"	=>	200,
+		]);
+
+		$url = "https://api.instagram.com/v1/users/self/feed";
+
+		$params = array();
+		if($val = $options["min"]) {
+			$params["min_id"] = $val;
+		}
+		if($val = $options["limit"]) {
+			$params["count"] = $val;
+		}
+
+		return $this->fetch($url,$params);
+
+	}
+
+
+}
