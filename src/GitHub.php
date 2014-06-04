327aee9a4574576480fe1217cc4e844d836dcfea
Craig Duncan <git@duncanc.co.uk>
2014-06-04 21:32:00 +0100

    Created a GitHub class based on OAuth2

---
+++ src/GitHub.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace duncan3dc\OAuth;
+use \duncan3dc\Helpers\Helper;
+use \duncan3dc\Helpers\Json;
+
+class GitHub extends OAuth2 {
+
+
+    public function __construct($options) {
+
+        $options = Helper::getOptions($options,[
+            "username"  =>  false,
+            "client"    =>  "",
+            "secret"    =>  "",
+            "code"      =>  "",
+        ]);
+
+        parent::__construct([
+            "type"          =>  "github",
+            "username"      =>  $options["username"],
+            "client"        =>  $options["client"],
+            "secret"        =>  $options["secret"],
+            "code"          =>  $options["code"],
+            "authoriseUrl"  =>  "https://github.com/login/oauth/authorize?scope=repo",
+            "redirectUrl"   =>  "http://developer.github.com/v3/",
+            "accessUrl"     =>  "https://github.com/login/oauth/access_token",
+        ]);
+
+    }
+
+
+    public function fetch($url,$data=false,$headers=false) {
+
+        if(!is_array($headers)) {
+            $headers = [];
+        }
+        $headers["Accept"] = "application/vnd.github.v3+json";
+        $headers["User-Agent"] = $this->username;
+
+        return parent::fetch($url,$data,$headers);
+
+    }
+
+
+    public function post($url,$data) {
+        global $system;
+
+        $headers = array(
+            "Accept"        =>  "application/vnd.github.v3+json",
+            "User-Agent"    =>  $this->username,
+        );
+
+        $url = $system->url($url,[
+            "access_token"  =>  $this->get("token"),
+        ]);
+
+        $json = Helper::curl([
+            "url"       =>  $url,
+            "headers"   =>  $headers,
+        ],Json::encode($data));
+
+        return Json::decode($json);
+
+    }
+
+
+}
