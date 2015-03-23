9d6f83941074feca999f9501a854623c5049a0a2
Craig Duncan <git@duncanc.co.uk>
2015-03-23 18:41:27 +0000

    Allow an Sql instance to be provided
    Instead of just a class that can be used for instantiation

--- src/Sql.php
+++ src/Sql.php
@@ -2,17 +2,29 @@
 
 namespace duncan3dc\OAuth;
 
-class Sql extends \duncan3dc\SqlClass\Sql
+use duncan3dc\SqlClass\Sql as SqlClass;
+
+class Sql extends SqlClass
 {
     protected static $class;
+    protected static $sql;
 
     public static function useClass($class)
     {
         static::$class = $class;
     }
 
+    public static function useInstance(SqlClass $sql)
+    {
+        static::$sql = $sql;
+    }
+
     public static function getInstance($server = null)
     {
+        if (static::$sql !== null) {
+            return static::$sql;
+        }
+
         if (static::$class) {
             return call_user_func(static::$class . "::getInstance", $server);
         } else {
